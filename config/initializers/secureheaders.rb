695e61a43de543d653691e2bd176c30f42c136bc
Chris Beer <cabeer@stanford.edu>
2017-12-11 07:58:58 -0800

    Disable HTTPS enforcement

--- config/initializers/secureheaders.rb
+++ config/initializers/secureheaders.rb
@@ -10,17 +10,11 @@ SecureHeaders::Configuration.default do |config|
   # thanks to sir-trevor..
   config.csp[:script_src] << '\'unsafe-eval\''
 
-  # thanks to google analytics
-  config.csp[:script_src] << '\'sha256-GFpwrduTw94NOGmBZ2ivuwcZIMsfyYQWMDV25hR82Ds=\''
-  config.csp[:script_src] << 'www.google-analytics.com'
-
-  # The metmuseum doesn't support https. :(
-  config.csp[:img_src] = ['*']
-
-  if Rails.env.development? || Rails.env.test?
+  # pending real SSL certificate:
+  # if Rails.env.development? || Rails.env.test?
     config.cookies[:secure] = SecureHeaders::OPT_OUT
     config.hsts = SecureHeaders::OPT_OUT
     config.csp[:default_src] << 'http:'
     config.csp[:script_src] << 'http:'
-  end
+  # end
 end
