cf1d489d2a24f22d6a1866b2892a0d8a1f6839c9
Chris Beer <chris@cbeer.info>
2018-05-14 15:37:13 -0700

    Remove unnecessary version number

--- system-setup/010-install_dependencies.sh
+++ system-setup/010-install_dependencies.sh
@@ -28,7 +28,7 @@ chkconfig ntpd on
 
 yum install -y pygpgme curl
 curl --fail -sSLo /etc/yum.repos.d/passenger.repo https://oss-binaries.phusionpassenger.com/yum/definitions/el-passenger.repo
-yum install -y mod_passenger || yum-config-manager --enable cr && yum install -y mod_passenger passenger-devel-5.3.0
+yum install -y mod_passenger || yum-config-manager --enable cr && yum install -y mod_passenger passenger-devel
 
 # Configure passenger
 echo "
