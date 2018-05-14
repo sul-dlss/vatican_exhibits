d56d1e3207502c2d64798a0063b53e94f172e171
Jack Reed <phillipjreed@gmail.com>
2018-05-14 15:30:03 -0500

    setup capistrano deploy

---
+++ config/deploy/stage.rb
@@ -0,0 +1,6 @@
+server 'vatican-dev.sul.stanford.edu', user: 'centos', roles: %w{app db web background}, my_property: :my_value
+
+set :rails_env, 'production'
+
+set :sidekiq_role, :background
+set :sidekiq_processes, 2
