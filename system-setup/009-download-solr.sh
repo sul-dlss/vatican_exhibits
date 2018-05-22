ef7d666b21437683b5120b612f8dcb2b58e9a698
Jessie Keck <jessie.keck@gmail.com>
2018-05-22 14:53:28 -0700

    Bump to solr version 7.3.1

--- system-setup/009-download-solr.sh
+++ system-setup/009-download-solr.sh
@@ -5,4 +5,4 @@ set -x #echo on
 
 # Download solr from a randomly chosen mirror
 # (Note: this will need updating after new solr releases)
-curl -O http://apache.claz.org/lucene/solr/7.3.0/solr-7.3.0.tgz
+curl -O http://apache.claz.org/lucene/solr/7.3.1/solr-7.3.1.tgz
