c9abe2ecd93a0814cddcc69c663cdaf13a013d6f
Chris Beer <cabeer@stanford.edu>
2018-05-31 14:44:58 -0700

    Add vatican deploy target

---
+++ config/deploy/vatican.rb
@@ -0,0 +1,13 @@
+ask(:user, 'deploy_user')
+ask(:server_name, 'server_name')
+ask(:port, 'ssh_port')
+
+set :deploy_to, '/srv/app'
+
+server fetch(:server_name), user: fetch(:user), roles: %w(web db app), port: fetch(:port)
+set :bundle_without, %w(test deployment development).join(' ')
+
+set :rails_env, 'production'
+
+set :sidekiq_role, :background
+set :sidekiq_processes, 2
