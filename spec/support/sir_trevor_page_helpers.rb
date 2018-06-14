c6ef4032d727e3a6c0b1bde3446b1d116a7f6238
Jack Reed <phillipjreed@gmail.com>
2018-06-14 14:35:33 -0600

    add ported over Spotlight SirTrevor autocomplete helpers

--- spec/support/sir_trevor_page_helpers.rb
+++ spec/support/sir_trevor_page_helpers.rb
@@ -1,5 +1,25 @@
 ## These methods are taken from Spotlght to help add sir-trevor widgets
 module SirTrevorPageHelpers
+  def fill_in_typeahead_field(opts = {})
+    type = opts[:type] || 'twitter'
+    # Poltergeist / Capybara doesn't fire the events typeahead.js
+    # is listening for, so we help it out a little:
+    page.execute_script <<-SCRIPT
+      $("[data-#{type}-typeahead]:visible").val("#{opts[:with]}").trigger("input");
+      $("[data-#{type}-typeahead]:visible").typeahead("open");
+      $(".tt-suggestion").click();
+    SCRIPT
+
+    find('.tt-suggestion', text: opts[:with], match: :first).click
+  end
+
+  # just like #fill_in_typeahead_field, but wait for the
+  # form fields to show up on the page too
+  def fill_in_solr_document_block_typeahead_field(opts)
+    fill_in_typeahead_field(opts)
+    expect(page).to have_css('li[data-resource-id="' + opts[:with] + '"]')
+  end
+
   def add_widget(type)
     click_add_widget
 
