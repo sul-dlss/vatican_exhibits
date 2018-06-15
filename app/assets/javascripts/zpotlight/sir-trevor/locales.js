a5b22ed64d64664867290ebaf53579a6932f6320
Andrew Winget (Standard) <scipioaffricanus@gmail.com>
2018-06-15 16:27:26 -0700

    add mirador modal and button to show modal

--- app/assets/javascripts/zpotlight/sir-trevor/locales.js
+++ app/assets/javascripts/zpotlight/sir-trevor/locales.js
@@ -3,6 +3,20 @@ SirTrevor.Locales.en.blocks = $.extend(SirTrevor.Locales.en.blocks, {
     caption_label: 'Viewer caption',
     description: 'This widget displays items in a Mirador viewer, which enables exhibit visitors to view and compare images in a zoomable, interactive environment. Optionally, you can add a heading and/or text to be displayed above the Mirador viewer and a caption to be displayed below the viewer.',
     heading_label: 'Heading',
+    mirador_modal: {
+      header: '',
+      instructions: '',
+      options_label: '',
+      options: {
+        enable_public_annotations: '',
+        display_by_default: '',
+        change_page: '',
+        adjust_zoom_level: '',
+        change_view_type: '',
+        show_hide_content_sidebar: '',
+        show_hide_thumbnail_strip: ''
+      }
+    },
     source_location: {
       exhibit: {
         label: 'This exhibit',
