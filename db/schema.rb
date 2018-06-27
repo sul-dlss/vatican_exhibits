7ccffea7a157a20689d38f40bb8cb58ef6a7603a
Jack Reed <phillipjreed@gmail.com>
2018-06-27 13:48:32 -0600

    update Spotlight to v2.1.0

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_29_221543) do
+ActiveRecord::Schema.define(version: 2018_06_27_194747) do
 
   create_table "annotot_annotations", force: :cascade do |t|
     t.string "uuid"
@@ -213,7 +213,7 @@ ActiveRecord::Schema.define(version: 2018_05_29_221543) do
     t.string "type"
     t.string "slug"
     t.string "scope"
-    t.text "content"
+    t.text "content", limit: 16777215
     t.integer "weight", default: 50
     t.boolean "published"
     t.integer "exhibit_id"
