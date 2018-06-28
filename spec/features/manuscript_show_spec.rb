a009ca70a5461cb0222073a6fa2693a8d240ddb9
Chris Beer <cabeer@stanford.edu>
2018-06-28 09:10:52 -0700

    Update tests to use new shelfmarks

--- spec/features/manuscript_show_spec.rb
+++ spec/features/manuscript_show_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe 'Manuscript display', type: :feature do
   let(:exhibit) { FactoryBot.create(:exhibit) }
 
   it 'has a general information section' do
-    visit spotlight.exhibit_solr_document_path(exhibit, 'MSS_Vat_gr_504')
+    visit spotlight.exhibit_solr_document_path(exhibit, 'Vat_gr_504')
     expect(page).to have_css 'h2', text: 'General information'
 
     within '.general-section' do
@@ -14,7 +14,7 @@ RSpec.describe 'Manuscript display', type: :feature do
   end
 
   it 'has a description section' do
-    visit spotlight.exhibit_solr_document_path(exhibit, 'MSS_Vat_gr_504')
+    visit spotlight.exhibit_solr_document_path(exhibit, 'Vat_gr_504')
     expect(page).to have_css 'h2', text: 'Description'
 
     within '.description-section' do
@@ -24,7 +24,7 @@ RSpec.describe 'Manuscript display', type: :feature do
   end
 
   it 'has parts' do
-    visit spotlight.exhibit_solr_document_path(exhibit, 'MSS_Vat_gr_504')
+    visit spotlight.exhibit_solr_document_path(exhibit, 'Vat_gr_504')
     expect(page).to have_css 'h2', text: 'Parts of this manuscript'
     expect(page).to have_css 'h3', text: '1r-4v'
     expect(page).to have_css 'h3', text: '5r-10r'
