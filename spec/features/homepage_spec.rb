3371c71c05b174ef1112dc3f7b33f9477ecd34f0
Jack Reed <phillipjreed@gmail.com>
2018-07-03 08:31:17 -0600

    add custom sidebar content

---
+++ spec/features/homepage_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe 'Homepage', type: :feature do
+  it 'has custom more information links' do
+    visit '/'
+    expect(page).to have_link 'Project background'
+    expect(page).to have_link 'Project staff'
+    expect(page).to have_link 'Vatican library'
+    expect(page).to have_link 'DigiVatLib'
+    expect(page).to have_link 'Online catalog'
+  end
+end
