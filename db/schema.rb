a5d8a09a0d177f83b067b688d274d5539d7f2e38
Camille Villa <cvilla100@gmail.com>
2018-08-16 13:37:14 -0700

    Update to Spotlight v2.2.0

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_27_194747) do
+ActiveRecord::Schema.define(version: 2018_08_14_221815) do
 
   create_table "annotot_annotations", force: :cascade do |t|
     t.string "uuid"
