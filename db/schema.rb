b0105db16ba8de0b81753d2dff25371b6dc60e05
Jack Reed <phillipjreed@gmail.com>
2018-09-20 15:24:29 +0200

    update Spotlight::Pages to use utf8mb for non latin charsets

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_14_221815) do
+ActiveRecord::Schema.define(version: 2018_09_19_132929) do
 
   create_table "annotot_annotations", force: :cascade do |t|
     t.string "uuid"
