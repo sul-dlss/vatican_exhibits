2710eae05e7d6f697de23b1051d294f1a154bdf3
Camille Villa <cvilla100@gmail.com>
2018-11-21 15:58:28 -0800

    Allow additional html in curatorial narrative

--- spec/helpers/spotlight_helper_spec.rb
+++ spec/helpers/spotlight_helper_spec.rb
@@ -5,6 +5,7 @@ RSpec.describe SpotlightHelper, type: :helper do
     let(:value) do
       <<-EOHTML
       <p><span style="font-size: 12.0pt; line-height: 115%; font-family: 'Calibri',sans-serif; mso-ascii-theme-font: minor-latin; mso-fareast-font-family: Calibri; mso-fareast-theme-font: minor-latin; mso-hansi-theme-font: minor-latin; mso-bidi-theme-font: minor-latin; mso-ansi-language: IT; mso-fareast-language: EN-US; mso-bidi-language: AR-SA;">La <em>r</em> e la <em>s</em> hanno forme molto simili con l&rsquo;unica differenza che, mentre la <em>r</em> ripiega bruscamente in forma angolosa, la <em>s</em> ha una forma pi&ugrave; tondeggiante.</span></p>
+      <p><b>Coffee</b> <i>crema</i> breve, <br>french press, turkish, cultivar java, whipped that qui pumpkin spice black. Rich, viennese, steamed as carajillo roast café au lait mazagran caffeine. Single origin so, fair trade eu, acerbic shop siphon acerbic robust brewed con panna.</p>
       EOHTML
     end
 
