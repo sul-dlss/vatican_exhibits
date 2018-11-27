9c87cd1ba8d4e279baa14daec7dadf14352330bb
Gary Geisler <ggeisler@gmail.com>
2018-11-27 12:42:38 -0700

    Remove two of the More Info sidebar links

--- spec/features/homepage_spec.rb
+++ spec/features/homepage_spec.rb
@@ -3,8 +3,6 @@ require 'rails_helper'
 RSpec.describe 'Homepage', type: :feature do
   it 'has custom more information links' do
     visit '/'
-    expect(page).to have_link 'Project background'
-    expect(page).to have_link 'Project staff'
     expect(page).to have_link 'Vatican library'
     expect(page).to have_link 'DigiVatLib'
     expect(page).to have_link 'Online catalog'
