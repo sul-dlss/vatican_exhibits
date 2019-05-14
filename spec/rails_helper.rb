39bc54de00c8f6c77d7f5d005fe1e5a9a662180f
Chris Beer <cabeer@stanford.edu>
2019-05-14 10:27:40 -0700

    Allow requests to chromedriver.storage.googleapis.com

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -9,7 +9,10 @@ require 'rspec/rails'
 require 'factory_bot_rails'
 # Add additional requires below this line. Rails is not loaded until this point!
 require 'webmock/rspec'
-WebMock.disable_net_connect!(allow_localhost: true)
+allowed_sites = [
+  'chromedriver.storage.googleapis.com'
+]
+WebMock.disable_net_connect!(allow_localhost: true, allow: allowed_sites)
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
